22e81e02f15dcacccfb5246e76852b0a65973a54
Max Smagin <lazlogogalac777@gmail.com>
2024-06-01 14:59:10 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development shell for catris";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+  outputs = { self, nixpkgs, poetry2nix, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { 
+        system = system;
+        config = {
+          allowUnfree = true;
+        }; 
+      };
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      catris = mkPoetryApplication { 
+        projectDir = ./.;
+        python = pkgs.python3;
+        pyproject = ./pyproject.toml;
+        poetryLock = ./poetry.lock; 
+      };
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+        buildInputs = with pkgs; [
+          catris
+        ];    
+      };
+  };
+}
